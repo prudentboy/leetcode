5fdbd61bde7f9ea619e847e6a1f4a1357ceafa78
hby <sogou-macpro@Sogou-MACProdeMacBook-Pro.local>
2020-08-24 15:47:13 +0800

    update 770's final state

--- Solutions/770.basic-calculator-iv.cpp
+++ Solutions/770.basic-calculator-iv.cpp
@@ -6,99 +6,163 @@
 
 // @lc code=start
 class Solution {
-    using Poly = multiset<string>;
-    using Polys = map<Poly, int, function<bool(const Poly&, const Poly&)>>;
 public:
     vector<string> basicCalculatorIV(string expression, vector<string>& evalvars, vector<int>& evalints) {
-        auto cmp = [](const Poly& left, const Poly& right) -> bool {
-            if (left.size() == right.size()) { return left < right; }
-            return left.size() < right.size();
-        };
-        Polys ret(cmp);
-
-        unordered_map<string_view, int> valUmp;
         const int m(evalvars.size());
-        for (int i(0); i < m; ++i) { valUmp[evalvars[i]] = evalints[i]; }
+        for (int i(0); i < m; ++i) { known_[evalvars[i]] = evalints[i]; }
 
-        string_view exp(expression);
+        Polys ret(basicCalculatorIV(expression));
+        return getAnswer(ret);
+    }
+private:
+    using Poly = vector<int>;
+    using Polys = map<Poly, int>;
+    Polys basicCalculatorIV(const string_view& exp) {
         stack<Polys> pStk;
-        stack<char> opStk;
-        for (int i(0); i < expression.size(); ++i) {
+        stack<char> operatorStk;
+        char pre('+');
+        operatorStk.push('+');
+
+        for (int i(0); i < exp.size(); ++i) {
+            // blank
+            if (exp[i] == ' ') { continue; }
             // digit
             if (isdigit(exp[i])) {
                 int num(exp[i++] - '0');
-                while (i < expression.size() && isdigit(exp[i])) {
+                while (i < exp.size() && isdigit(exp[i])) {
                     num *= 10;
                     num += exp[i++] - '0';
                 }
                 --i;
-                if (num == 0) continue;
-                pStk.push({"", num});
+                cout << num << endl;
+                Polys tmp({{}, num});
+                if (!operatorStk.empty() && operatorStk.top() == '*') {
+                    multi(tmp, pStk.top());
+                    operatorStk.pop();
+                }
+                pStk.push(move(tmp));
                 continue;
             }
             // variable
-            if (isalpha(expression[i])) {
+            if (isalpha(exp[i])) {
                 int beg(i), len(1);
-                while (i < expression.size() && isalpha(exp[i])) {
+                while (i < exp.size() && isalpha(exp[i])) {
                     ++i;
                     ++len;
                 }
                 --i;
-                string_view ex(expression.substr(beg, len));
-                pStk.push(valUmp.count(ex) > 0 ? {"", valUmp[ex]} : {ex, 1});
+                string_view var(exp.substr(beg, len));
+                cout << var << endl;
+                Polys tmp;
+                if (known_.count(var) > 0) {
+                    tmp[{}] = known_[var];
+                } else {
+                    if (fv_idx_.count(var) == 0) {
+                        vars_.push_back(var);
+                        fv_idx_[var] = vars_.size() - 1;
+                    }
+                    vector<int> k(fv_idx_[var] + 1);
+                    k[fv_idx_[var]] = 1;
+                    tmp[k] = 1;
+                }
+                if (!operatorStk.empty() && operatorStk.top() == '*') {
+                    multi(tmp, pStk.top());
+                    operatorStk.pop();
+                }
                 continue;
             }
-
+            // oprators
+            if (exp[i] == '+') {
+                operatorStk.push('+');
+            } else if (exp[i] == '-') {
+                operatorStk.push('-');
+            } else if (exp[i] == '*') {
+                operatorStk.push('*');
+            } else if (exp[i] == '(') {
+                operatorStk.push('(');
+            } else if (exp[i] == ')') {
+                Polys tmp;
+                tmp[{}] = 0;
+                while (operatorStk.top() != '(') {
+                    calculate(tmp, pStk.top(), operatorStk.top());
+                    operatorStk.pop();
+                    pStk.pop();
+                }
+                operatorStk.pop();
+                pStk.push(tmp);
+            }
         }
-        return getAnswer(ret);
+        Polys ret;
+        ret[{}] = 0;
+        while (!pStk.empty()) {
+            calculate(ret, pStk.top(), operatorStk.top());
+            operatorStk.pop();
+            pStk.pop();
+        }
+        return ret;
     }
-private:
-    Polys add(Polys& x, Polys& y) {
+    void calculate(Polys& x, Polys& y, char op) {
+        switch (op) {
+            case '+' :
+                add(x, y);
+                break;
+            case '-' :
+                minus(x, y);
+                break;
+            case '*' :
+                multi(x, y);
+                break;
+        }
+    }
+    void add(Polys& x, Polys& y) {
         for (auto& p : y) {
             auto iter(x.find(p.first));
             if (iter == x.end()) {
                 x.insert(p);
             } else {
                 iter->second += p.second;
-                if (iter->second == 0) { x.erase(iter); }
             }
         }
-        return x;
     }
-    Polys minus(Polys& x, Polys& y) {
+    void minus(Polys& x, Polys& y) {
         for (auto& p : y) {
             auto iter(x.find(p.first));
             if (iter == x.end()) {
                 x.insert(p);
             } else {
-                iter->second += p.second;
-                if (iter->second == 0) { x.erase(iter); }
+                iter->second -= p.second;
             }
         }
-        return x;
     }
-    Polys multiply(Polys& x, Polys& y) {
+    void multi(Polys& x, Polys& y) {
         Polys ret;
         for (auto& xx : x) {
             for (auto& yy : y) {
                 int coef(xx.second * yy.second);
-                Poly nxtPoly(xx.first);
-                for (auto& s : yy.first) { nxtPoly.insert(s); }
-                auto iter(ret.find(nxtPoly));
+                vector<int> k(vars_.size());
+                for (int i(0); i < xx.first.size(); ++i) { k[i] += xx.first[i]; }
+                for (int i(0); i < yy.first.size(); ++i) { k[i] += yy.first[i]; }
+                auto iter(ret.find(k));
                 if (iter == ret.end()) {
-                    ret[nxtPoly] = coef;
+                    ret[k] = coef;
                 } else {
                     iter->second += coef;
-                    if (iter->second == 0) { ret.erase(iter); }
-                } 
+                }
             }
         }
-        return ret;
+        swap(x, ret);
     }
     vector<string> getAnswer(const Polys& ps) {
         vector<string> ans;
+        /*
         for (auto& p : ps) {
+            if (p.second == 0) { continue; }
             string tmp(to_string(p.second));
+            vector<string_view> fv;
+            for (int i(0); i < p.first.size(); ++i) {
+                while (p.first[i]-- > 0) { fv.push_back(vars_[i]); }
+            }
+            sort(fv.begin(), fv.end(), [](const string))
             for (auto& str : p.first) {
                 if (str.empty()) { continue; }
                 tmp += '*';
@@ -106,8 +170,13 @@ private:
             }
             ans.emplace_back(move(tmp));
         }
+        */
         return ans;
     }
+private:
+    vector<string_view> vars_;
+    unordered_map<string_view, int> fv_idx_;
+    unordered_map<string_view, int> known_;
 };
 // @lc code=end
 
--- Solutions/tmp.cpp
+++ Solutions/tmp.cpp
@@ -1,267 +1,114 @@
-typedef int Coefficient;
-typedef multiset<string> Polynomial;
-struct Cmp
-{
-	bool operator()(const Polynomial &a, const Polynomial&b)const
-	{
-		if (a.size() < b.size())//次数高的放前面
-			return false;
-		else if (a.size() > b.size())
-			return true;
-		else
-			return a < b;//次数相同，字典序小的放前面
-	}
-};
-typedef map<Polynomial, Coefficient,Cmp> Polynomials;
-typedef pair<string, int> E_Value_Pair;
 class Solution {
 public:
-	vector<string> basicCalculatorIV(string expression, vector<string> &evalvars, vector<int> &evalints) {
-
-		stack<Polynomials>opd;//放操作数
-		stack<char>opr;//放运算符
-		char c;
-		char pre_opr;
-		char pre_c='#';
-		expression += "#";
-		unordered_map<string,int>e_value_map;
-		for (int i = 0; i < evalvars.size(); i++)
-		{
-			e_value_map.insert(pair<string,int>(evalvars[i], evalints[i]));
-		}
-
-		//opd.push(Polynomials());
-		opr.push('#');
-		int max_len = expression.size();
-
-		for (int i = 0; i < max_len; i++)
-		{
-			c = expression[i];
-			pre_opr = opr.top();
-			
-			if (c == '+'||c=='-'||c==')'||c=='#')//要考虑作为数字的一元运算符
-			{
-				while(pre_opr == '+' || pre_opr == '-'|| pre_opr == '*' || pre_opr == '/')
-				{ 
-					opr.pop();
-					if(pre_opr=='+')
-					{
-						auto top = opd.top();
-						if (pre_c != '#' && pre_c != '(')//只有在作为二元运算符的时候才考虑
-						{
-							opd.pop();
-							add(opd.top(), top);
-						}
-					}
-					else if (pre_opr == '-')
-					{
-						auto top = opd.top();
-						if (pre_c != '#' && pre_c != '(')//2元运算
-						{
-							opd.pop();
-							sub(opd.top(), top);
-						}
-						else
-						{
-							multi(opd.top(), Polynomials{ {Polynomial{""},-1} });//相当于乘上-1
-						}
-					}
-					else if (pre_opr == '*')
-					{
-						auto top = opd.top();
-						opd.pop();
-						multi(opd.top(), top);
-					}
-					else if (pre_opr == '/')
-					{
-						auto top = opd.top();
-						opd.pop();
-						div(opd.top(), top);
-					}
-					pre_opr = opr.top();
-				}
-				if (pre_opr == '('&&c==')')
-					opr.pop();
-				else
-					opr.push(c);
-			}
-			else if (c == '*' || c == '/' )
-			{
-				while ( pre_opr == '*' || pre_opr == '/')
-				{
-					if (pre_opr == '*')
-					{
-						auto top = opd.top();
-						opd.pop();
-						multi(opd.top(), top);
-					}
-					else if (pre_opr == '/')
-					{
-						auto top = opd.top();
-						opd.pop();
-						div(opd.top(), top);
-					}
-					opr.pop();
-					pre_opr = opr.top();
-				}
-					opr.push(c);
-			}
-			else if (c >= '0'&&c <= '9')//常数项作为空串
-			{
-				int start = i;
-				int len = 0;
-				for (; i < max_len&&expression[i] >= '0'&&expression[i] <= '9'; i++, len++);
-				string str = expression.substr(start, len);
-				int num;
-				sscanf(str.c_str(), "%d", &num);
-				//if(num!=0)
-					opd.push(Polynomials{ {Polynomial(),num} });//常数项不应该设为0
-				i--;
-
-			}
-			else if (c >= 'a'&&c <= 'z')//处理单个表达式
-			{
-				int start = i;
-				int len = 0;
-				for (; i < max_len&&expression[i] >= 'a'&&expression[i] <= 'z'; i++, len++);
-				string name = expression.substr(start, len);
-				unordered_map<string, int>Num;
-				auto iter = e_value_map.find(name);
-				if (iter == e_value_map.end())//非常数项
-				{
-					opd.push(Polynomials{ {Polynomial{name},1} });
-				}
-				else//在 当作常数项
-				{
-					//if(iter->second!=0)
-						opd.push(Polynomials{ {Polynomial(),iter->second} });
-				}
-				i--;
-
-			}
-			else if (c == '(')
-			{
-				opr.push('(');
-				//opd.push(Polynomials());
-			}
-			if (c!=' ')
-				pre_c = c;
-		}
-		//print(opd.top());
-		if (opd.empty())
-			return vector<string>();
-		else 
-			return get_result(opd.top());
-	}
-	void add(Polynomials&a, const Polynomials&b)//把b都放到a里面
-	{
-		for (auto p : b)
-		{
-			auto iter = a.find(p.first);
-			if (iter == a.end())
-			{
-				a.insert(p);
-			}
-			else if(iter->second+p.second==0)
-			{
-				a.erase(iter);
-			}
-			else
-			{
-				iter->second += p.second;
-			}
-		}
-	}
-
-	void sub(Polynomials&a, const Polynomials&b)
-	{
-		for (auto p : b)
-		{
-			auto iter = a.find(p.first);
-			if (iter == a.end())
-			{
-				p.second = -p.second;
-				a.insert(p);
-			}
-			else if (iter->second - p.second == 0)
-			{
-				a.erase(iter);
-			}
-			else
-			{
-				iter->second -= p.second;
-			}
-		}
-	}
-	void multi(Polynomials&a, const Polynomials&b)
-	{
-		Polynomials ret;
-		for (auto pb : b)
-		{
-			if (pb.second == 0)
-				continue;
-			for (auto pa : a)
-			{
-				if (pa.second == 0)
-					continue;
-				Coefficient co = pa.second*pb.second;
-				Polynomial tmp = pa.first;
-				//merge 函数时c++17的，所以得自己实现
-				for (auto word : pb.first)
-				{
-					tmp.insert(word);
-				}
-				
-				add(ret, Polynomials{ {tmp,co} });
-			}
-		}
-		a=ret;//貌似也不是很复杂嘛？搞错了
-	}
-	void div(Polynomials&a, const Polynomials&b)
-	{
-		Polynomials ret;
-		for (auto p : b)
-		{
-			Polynomials tmp{ p };
-			add(ret, tmp);
-		}
-		a = ret;//貌似也不是很复杂嘛？
-	}
-	void print(Polynomials&ps)
-	{
-		for (auto p : ps)
-		{
-			cout << p.second;
-			for (auto little_p : p.first)
-			{
-				cout << "*" << little_p.c_str();
-			}
-				
-			cout << endl;
-		}
-	}
-	vector<string> get_result(Polynomials&ps)
-	{
-//这里仔细看一下对0的处理，你可能纳闷了，加减乘不是处理了0的问题了吗，
-//但是还有一个问题，你直接读入一个0或者某个word的值是0，
-//你就会插入到栈里面，我试过不插入，但好麻烦，所以我直接在最后处理好了。
-		vector<string> ret;
-		for (auto p : ps)
-		{
-			string line;
-			//cout << p.second;
-			if(p.second!=0)
-				line +=to_string(p.second);
-			for (auto little_p : p.first)
-			{
-				//cout << "*" << little_p.c_str();
-				line += "*" + little_p;
-			}
-			if(!line.empty())
-				ret.push_back(line);
-			//cout << endl;
-		}
-		return ret;
-	}
-	
+    vector<string> basicCalculatorIV(string expression, vector<string>& evalvars, vector<int>& evalints) {
+        unordered_map<string, int> mp;
+        int n = evalvars.size();
+        // create a map for variable value pairs
+        for (int i = 0; i < n; ++i) mp[evalvars[i]] = evalints[i];
+        // helper function is recursion using implicit stack
+        int pos = 0;
+        unordered_map<string, int> output = helper(expression, mp, pos);
+        vector<pair<string, int>> ans(output.begin(), output.end());
+        // sort result based on variable degree
+        sort(ans.begin(), ans.end(), mycompare);
+        vector<string> res;
+        for (auto& p: ans) {
+            // only consider non-zero coefficient variables
+            if (p.second == 0) continue;
+            res.push_back(to_string(p.second));
+            if (p.first != "") res.back() += "*"+p.first;
+        }
+        return res;
+    }
+private:
+    unordered_map<string, int> helper(string& s, unordered_map<string, int>& mp, int& pos) {
+        // every operand is an unordered_map, including single variable or nested (a * b + a * c); 
+        // if the operand is a number, use pair("", number)
+        vector<unordered_map<string, int>> operands;
+        vector<char> ops;
+        ops.push_back('+');
+        int n = s.size();
+        while (pos < n && s[pos] != ')') {
+            if (s[pos] == '(') {
+               pos++;
+               operands.push_back(helper(s, mp, pos));
+            }
+            else {
+               int k = pos;
+               while (pos < n && s[pos] != ' ' && s[pos] != ')') pos++;
+               string t = s.substr(k, pos-k);
+               bool isNum = true;
+               for (char c: t) {
+                   if (!isdigit(c)) isNum = false;
+               }
+               unordered_map<string, int> tmp;
+               if (isNum) 
+                   tmp[""] = stoi(t);
+               else if (mp.count(t)) 
+                   tmp[""] = mp[t];
+               else              
+                   tmp[t] = 1;
+               operands.push_back(tmp);
+            }
+            if (pos < n && s[pos] == ' ') {
+               ops.push_back(s[++pos]);
+               pos += 2;
+            }
+        }
+        pos++;
+        return calculate(operands, ops);
+    }
+    unordered_map<string, int> calculate(vector<unordered_map<string, int>>& operands, vector<char>& ops) {
+        unordered_map<string, int> ans;
+        int n = ops.size();
+        for (int i = n-1; i >= 0; --i) {
+            unordered_map<string, int> tmp = operands[i];
+            while (i >= 0 && ops[i] == '*')
+                tmp = multi(tmp, operands[--i]);
+            int sign = ops[i] == '+'? 1: -1;
+            for (auto& p: tmp) ans[p.first] += sign*p.second;
+        }
+        return ans;
+    }
+    unordered_map<string, int> multi(unordered_map<string, int>& lhs, unordered_map<string, int>& rhs) {
+        unordered_map<string, int> ans;
+        int m = lhs.size(), n = rhs.size();
+        for (auto& p: lhs) {
+            for (auto& q: rhs) {
+                // combine and sort the product of variables
+                string t = combine(p.first, q.first);
+                ans[t] += p.second*q.second;
+            }
+        }
+        return ans;
+    }
+    string combine(const string& a, const string& b) {
+        if (a == "") return b;
+        if (b == "") return a;
+        vector<string> strs = split(a, '*');
+        for (auto& s: split(b, '*')) strs.push_back(s);
+        sort(strs.begin(), strs.end());
+        string s;
+        for (auto& t: strs) s += t +'*';
+        s.pop_back();
+        return s;
+    }
+    static vector<string> split(const string& s, char c) {
+        vector<string> ans;
+        int i = 0, n = s.size();
+        while (i < n) {
+            int j = i;
+            i = s.find(c, i);
+            if (i == -1) i = n;
+            ans.push_back(s.substr(j, i-j));
+            i++;
+        }
+        return ans;
+    }
+    static bool mycompare(pair<string, int>& a, pair<string, int>& b) {
+        string s1 = a.first, s2 = b.first;
+        vector<string> left = split(s1, '*'); 
+        vector<string> right = split(s2, '*');
+        return left.size() > right.size() || (left.size() == right.size() && left < right);
+    }  
 };
